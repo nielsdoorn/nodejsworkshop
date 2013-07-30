9a13f5372dce27991ba186da3fefc0208b87086d
Niels Doorn <nielsdoorn@gmail.com>
2013-07-30 21:39:24 +0200

    a little bit further with the lab and presentation

---
+++ tutorial/code/lab/twitter.js
@@ -0,0 +1,24 @@
+var twitter = require('ntwitter');
+
+var twit = new twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_TOKEN_SECRET
+});
+
+function queryTwitter(q) { 
+  twit.stream('statuses/filter', {'track':q}, function(stream) {
+    stream.on('data', function (data) {
+      if (data != undefined && data.user != undefined) {
+        console.log('tweet', data.user.screen_name+' tweets: '+data.text);
+      }
+    });
+
+    stream.on('error', function(error) {
+      console.log('error', error);
+    });
+  });
+};
+
+queryTwitter("now playing");
