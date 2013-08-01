11f5562df8d387a9cf8c0b1c10e7102382748fe3
Niels Doorn <nielsdoorn@gmail.com>
2013-08-01 10:40:45 +0200

    lab done

---
+++ tutorial/code/lab/public/client.js
@@ -0,0 +1,14 @@
+var socket = io.connect();
+
+var tweetList;
+window.onload = function() {
+	tweetList = document.createElement('ul');
+	document.body.appendChild(tweetList);
+}
+
+socket.on('tweet', function (tweet) {
+	var tweetListItem = document.createElement('li');
+	tweetListItem.innerHTML = tweet;
+	tweetList.appendChild(tweetListItem);
+});
+
