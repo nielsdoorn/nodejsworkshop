fab1d6c17f987488ef828816d2084cf4e7e4ddbd
Niels Doorn <nielsdoorn@gmail.com>
2014-01-18 15:56:45 +0100

    update for the weather exercise

--- tutorial/code/weather/weather.js
+++ tutorial/code/weather/weather.js
@@ -10,5 +10,6 @@ forecast = new Forecast(options);
 
 forecast.get(51.513417, 7.456795, function (err, res, data) {
   if (err) throw err;
-  console.log(util.inspect(data.hourly.summary));
+  console.log(util.inspect(data.daily.summary));
+
 });
